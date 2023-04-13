acc97bdb3330b1259dcb8d3fa3a6b0608b3733fb
Mohamed Refaat <63193470+moRefaat271@users.noreply.github.com>
2023-04-13 03:22:36 +0200

    MilestoneRepo + ProjectPostApplicantRepo

---
+++ HireMeDAL/Repos/Milestone/MilestoneRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireMeDAL
+{
+    public class MilestoneRepo : IMilestoneRepo
+    {
+        private readonly HireMeContext _hireMeContext;
+
+        public MilestoneRepo(HireMeContext hireMeContext)
+        {
+            this._hireMeContext = hireMeContext;
+        }
+        public void CreateMilestone(Milestone milestone)
+        {
+            _hireMeContext.milestones.Add(milestone);
+            SaveChanges();
+        }
+
+        public void DeleteMilestone(int id)
+        {
+            var milestone = _hireMeContext.milestones.Find(id);
+            if (milestone != null)
+            {
+                _hireMeContext.milestones.Remove(milestone);
+                SaveChanges();
+            }
+        }
+
+        public List<Milestone> GetALl()
+        {
+            return _hireMeContext.milestones.ToList();
+        }
+
+        public Milestone GetMilestoneById(int id)
+        {
+            return _hireMeContext.milestones.Find(id) ?? null;
+        }
+
+        public int SaveChanges()
+        {
+            return _hireMeContext.SaveChanges();
+        }
+
+        public void UpdateMilestone(int id, Milestone milestone)
+        {
+            var currentMilestone = GetMilestoneById(id);
+            if (currentMilestone != null)
+            {
+                currentMilestone.ProjectPost = milestone.ProjectPost;
+                currentMilestone.ProjectPostId = milestone.ProjectPostId;
+                currentMilestone.Name = milestone.Name;
+                currentMilestone.Value = milestone.Value;
+                currentMilestone.Id = milestone.Id;
+                SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ HireMeDAL/Repos/ProjectPostApplicant/ProjectPostApplicantRepo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireMeDAL
+{
+    public class ProjectPostApplicantRepo : IProjectPostApplicantRepo
+    {
+        private readonly HireMeContext _hireMeContext;
+
+        public ProjectPostApplicantRepo(HireMeContext hireMeContext)
+        {
+            this._hireMeContext = hireMeContext;
+        }
+        public void CreateProjectPostApplicant(ProjectPostApplicant projectPostApplicant)
+        {
+            _hireMeContext.projectPostApplicants.Add(projectPostApplicant);
+            SaveChanges();
+        }
+
+        public void DeleteProjectPostApplicant(int id)
+        {
+            var projectPostApplicant = _hireMeContext.projectPostApplicants.Find(id);
+            if (projectPostApplicant != null)
+            {
+                _hireMeContext.projectPostApplicants.Remove(projectPostApplicant);
+                SaveChanges();
+            }
+        }
+
+        public List<ProjectPostApplicant> GetALl()
+        {
+            return _hireMeContext.projectPostApplicants.ToList();
+        }
+
+        public ProjectPostApplicant GetProjectPostApplicantById(int id)
+        {
+            return _hireMeContext.projectPostApplicants.Find(id) ?? null;
+        }
+
+        public int SaveChanges()
+        {
+            return _hireMeContext.SaveChanges();
+        }
+
+        public void UpdateProjectPostApplicant(int id, ProjectPostApplicant projectPostApplicant)
+        {
+            var currentprojectPostApplicant = GetProjectPostApplicantById(id);
+            if (currentprojectPostApplicant != null)
+            {
+                currentprojectPostApplicant.BiddingPrice = projectPostApplicant.BiddingPrice;
+                currentprojectPostApplicant.ProjectPost = projectPostApplicant.ProjectPost;
+                currentprojectPostApplicant.PP_ID = projectPostApplicant.PP_ID;
+                currentprojectPostApplicant.FreelancerId = projectPostApplicant.FreelancerId;
+                currentprojectPostApplicant.Freelancer = projectPostApplicant.Freelancer;
+                currentprojectPostApplicant.Proposal = projectPostApplicant.Proposal;
+                SaveChanges();
+            }
+        }
+
+    }
+}

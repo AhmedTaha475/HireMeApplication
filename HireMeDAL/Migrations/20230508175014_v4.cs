b9cf4a1432214aa52460dcf1a42c057d6f530aba
AhmedTaha475 <76524115+AhmedTaha475@users.noreply.github.com>
2023-05-08 20:50:54 +0300

    UpdateMileStoneTable

---
+++ HireMeDAL/Migrations/20230508175014_v4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HireMeDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class v4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "released",
+                table: "milestones",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "released",
+                table: "milestones");
+        }
+    }
+}
